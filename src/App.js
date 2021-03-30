8d51b302ec90d2255c5ac86e13ca4b3e1698f138
Max <mpiroddi@outlook.com>
2021-03-30 16:19:13 +1100

    Commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Howdy Lee</h1>
+        <h1>Lets see if AWS Amplify is any good.</h1>
       </header>
     </div>
   );
